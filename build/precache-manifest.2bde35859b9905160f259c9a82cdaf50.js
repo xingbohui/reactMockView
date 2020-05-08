3a6031e415b587963cd35a263a8e24e9fbedbfa3
邢博辉 <bhxing@travelsky.com>
2020-05-08 14:27:28 +0800

    reactMockView

---
+++ build/precache-manifest.2bde35859b9905160f259c9a82cdaf50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c4d517c59a2073b127a127da986265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4c063663a7ef9274f5e",
+    "url": "/static/css/main.edb7952c.chunk.css"
+  },
+  {
+    "revision": "879f345bd7fad6b87414",
+    "url": "/static/js/2.4de3c545.chunk.js"
+  },
+  {
+    "revision": "d4c063663a7ef9274f5e",
+    "url": "/static/js/main.8c9ffec0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
